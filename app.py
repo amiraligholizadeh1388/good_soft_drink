82f82ff4a039ccb349fa67c89e5cae5d64404ff4
amirali <behrouz.gholizadeh98@gmail.com>
2024-10-04 11:20:36 +0330

    finished sign up part

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, Response, render_template, redirect, url_for, request, 
 from flask_sqlalchemy import SQLAlchemy
 import cv2
 import os
-
+import numpy as np
+from datetime import timedelta
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
+app.config['SESSION_PERMANENT'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -31,18 +34,58 @@ def start_capture():
     new_user = User(first_name=first_name, last_name=last_name, phone_number=phone_number, email=email)
     db.session.add(new_user)
     db.session.commit()
-
+    session['user_id'] = new_user.id
     return redirect(url_for('video_feed', user_id=new_user.id))
 
 @app.route('/video_feed/<int:user_id>')
 def video_feed(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('video_feed.html', user_id=user.id)
+
+@app.route('/video_feed_source')
+def video_feed_source():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def prepare_training_data(dataset_path):
+    faces = []
+    labels = []
+    label_dict = {}
+
+    for image_name in os.listdir(dataset_path):
+        image_path = os.path.join(dataset_path, image_name)
+        gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+        # Extract the person ID from the filename
+        person_id = int(image_name.split('_')[0])
+        label_dict[person_id] = person_id
+
+        faces.append(gray_image)
+        labels.append(person_id)
+
+    return faces, np.array(labels), label_dict
+
+
+def train():
+    dataset_path = 'face_dataset'
+
+    # Initialize the face recognizer
+
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    faces, labels, label_dict = prepare_training_data(dataset_path)
+    recognizer.train(faces, labels)
+
+    # Save the trained model
+    recognizer.save('trained_face_model.yml')
 
 def generate_frames():
     cap = cv2.VideoCapture(0)
     haar_cascade_path = 'cascades/haarcascade_frontalface_default.xml'
     face_cascade = cv2.CascadeClassifier(haar_cascade_path)
-    
+    person_id = 1
+    count = 0
+
     while True:
         success, frame = cap.read()
         if not success:
@@ -52,16 +95,25 @@ def generate_frames():
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         for (x, y, w, h) in faces:
+            count += 1
+            face = gray[y:y + h, x:x + w]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
+            face_filename = os.path.join('face_dataset', f"{person_id}_{count}.jpg")
+            cv2.imwrite(face_filename, face)
         # Encode the frame in JPEG format
         _, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
         
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        train()
+        if count == 30:
+            yield (b'--f\r\n'
+                            b'Content-Type: text/plain\r\n\r\n'
+                            b'REDIRECT\r\n\r\n')
+            break
     cap.release()  # Release the camera at the end
+    
 
 @app.route('/account')
 def account():
@@ -70,6 +122,7 @@ def account():
         return redirect(url_for('signing'))  # Redirect to signing if no user_id
     user = User.query.get(user_id)
     return render_template('account.html', first_name=user.first_name)
+
 
 if __name__ == '__main__':
     # Create database tables within an application context
